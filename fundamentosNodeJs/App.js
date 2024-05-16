9013202965a862dfbebb8c7b019940a0a2cb7bd7
marcosamado <led_9dejulio@hotmail.com>
2024-05-16 15:26:55 -0300

    inciando node js

---
+++ fundamentosNodeJs/App.js
@@ -0,0 +1,6 @@
+const frutas = ["banana", "banana", "pera", "banana"];
+const precio = [100, 200, 300];
+module.exports = {
+    frutas,
+    precio,
+};
---
+++ fundamentosNodeJs/Index.js
@@ -0,0 +1,9 @@
+const { frutas, precio } = require("./App");
+
+frutas.forEach((fruta) => {
+    console.count(fruta);
+});
+
+precio.forEach((precio) => {
+    console.log(precio);
+});
