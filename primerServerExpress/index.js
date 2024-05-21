c6cc9ae8ed9909447809f1f42815ac8793050c38
marcosamado <led_9dejulio@hotmail.com>
2024-05-20 21:28:25 -0300

    fundamentos de express

--- primerServerExpress/index.js
+++ primerServerExpress/index.js
@@ -1,6 +1,34 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const fs = require("fs");
+const { fileURLToPath } = require("url");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// app.get("/formulario", (req, res) => {
+//     console.log(req.query);
+//     res.send("Formulario enviado...");
+// });
+
+app.post("/formulario", (req, res) => {
+    console.log(req.body);
+
+    const { nombre, apellido } = req.body;
+
+    const data = `Nombre: ${nombre}\nApellido: ${apellido} `;
+    const dir = "public/archivos";
+    const filePath = `${dir}/DatosFormulario.txt`;
+
+    if (!nombre || !apellido) return res.redirect("/error.html");
+    fs.writeFile(filePath, data, (error) => {
+        if (error) return res.send("Fallo al crear el archivo");
+
+        // res.send("Se creo el archivo con exito");
+        res.download(__dirname + `/${filePath}`);
+    });
+});
 
 app.get("/", (req, res) => {
     res.send("visitaste la pagina de inicio");
